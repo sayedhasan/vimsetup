888fab72fa3ffccc44d95b5b2c810e6341e8d62a
Sayed Hasan <sayed.hasan@intel.com>
2014-12-01 00:31:25 -0800

    fixing a typo that caused failure in downloading vundle

--- bootstrap.pl
+++ bootstrap.pl
@@ -8,7 +8,6 @@ my $HTTP_PROXY = "http_proxy=http://proxy.fm.intel.com:911";
 # --- bootstrap vundle
 print "getting bundle ...\n";
 system( "git clone http://github.com/gmarik/Vundle.vim.git bundle/Vundle.vim" );
-"system( "git clone http://github.com/gmarik/vundle.git bundle/vundle" );
 if ($? ne '0') {
   print "failed to clone vundle. Try using it manually.\n";
   print "git clone http://github.com/gmarik/vundle.git\n";
